d0fd72efd4c321eda5fb613a9c6348757247dd97
Isandre47 <Isandre47@users.github.com>
2018-10-08 21:10:27 +0200

    Solution objectif 1

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <?php
 
+// appel des classes collier et chat
 require 'collar.php';
 require 'cat.php';
 
+// création d'un objet collier
 $collier = new \Animals\Collar('8', 'bleu');
+
+// création d'un objet chat nommé félix sans collier, ensuite une image et un niveau de fatigue
 $felix  = new \Animals\Cat('Oliver', "noir");
 $felix->setImage('imagechat');
 $felix->setFatigue('25');
 
+// création d'un objet chat nommé mistigri avec un collier, ensuite une image et un niveau de fatigue
 $mistigri = new \Animals\Cat('stef', 'jaune', $collier);
-$mistigri->setImage('gechat');
+$mistigri->setImage('');
 $mistigri->setFatigue('8');
 
 
-$felix->__toString();
-echo $felix;
+echo $felix->__toString();
 
 echo $mistigri->__toString();
-
 
  $felix->walk();
  $felix->walk();
@@ -30,7 +33,6 @@ echo $mistigri->__toString();
 if ($felix->isTired()){
     $felix->rest();
 }
-
 
 echo $felix->__toString();
 
